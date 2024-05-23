409bd740446ddfdf1add46bc698989c8c46612d5
mohamedfahil <mfahil26@gmail.com>
2024-05-24 00:28:39 +0530

    Add files via upload

---
+++ TaskDay5-FSD60WD/js/script.js
@@ -0,0 +1,49 @@
+let myResume={
+    "basics": {
+      "name": "MOHAMED FAHIL",
+      "email": "mfahil26@gamil.com",
+      "phone": 9578765687,
+      "degree": "B.Sc",
+      "location": {
+        "address": "15/16 Sainath Enclave ShenoyNagar",
+        "postalCode": 620020,
+        "city": "Trichy",
+        "state": "Tamilnadu",
+        "country": "India"
+      },
+    },
+    
+      
+    
+    "education": [
+      {
+        "institution": "Jamal Mohamed College",
+        "department": "B.Sc(Information,technology)",
+        "studyType": "fulltime",
+        "batch start year": 2021,
+        "batch end year": 2024,
+        "gpa": 7.5,
+      }
+    ],
+    "skills": [
+      {
+        "name": "javascript,UI/UX",
+        "level": "beginer",
+        "project": [
+          "UI/UX Design in figma"
+        ]
+      }
+    ],
+    "languages": [
+      {
+        "language": "Tamil,Enlish",
+      }
+    ],
+    "interests": [
+      {
+        "name": "FullstackDeveloper,EthicalHacking,",
+      }
+    ]
+  }
+  console.log(myResume);
+  
